297f5c56c77fea466aa22ee9396e0173ff3d4120
SSD1CRT <92155128+SSD1CRT@users.noreply.github.com>
2022-01-12 13:19:46 +0000

    Update edit.blade.php

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,119 +1,48 @@
-@extends('layouts.admin')
-@section('page_header')
-<ul class="navbar-nav flex-row">
-    <li>
-        <div class="page-header">
-            <div class="page-title">
-                <h3>Pages</h3>
-            </div>
+<?php $required_span = '<span class="text-red">*</span>'; ?>
+<div class="modal-dialog modal-md">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="EditModalLabel">{{trans('label.Edit')}} {{trans('label.Categories')}}</h5>
+            <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    </li>
-</ul>
-@endsection
-@section('content')
-<?php $required_span = '<span class="text-red">*</span>';?>
-<div class="layout-px-spacing">
-    <div class="row layout-top-spacing">
-        <div class="col-xl-12 col-lg-12 col-md-12 col-12 layout-spacing ">
-            <div class="statbox widget box box-shadow">
-                <div class="widget-header">
-                    <div class="row">
-                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                            <h4>Update About Us</h4>
-                        </div>
-                    </div>
+        <div class="modal-body">
+            <form class="form-horizontal editCategory" method="post" action="javascript:void(0)" >
+                {{ method_field('PUT') }}
+                <div class="mb-3">
+                    <label for="category_name" class="col-form-label"> Name:<?=$required_span; ?></label>
+                    <input class="form-control" type="text" name="category_name" id="category_name" value="{{ $row->category_name }}" required>
                 </div>
-                <div class="widget-content widget-content-area p-4">
-                    <div class="widget-one">
-                        <form method="POST" action="{{ route('admin.pages.update',$row['id']) }}" class="form-horizontal add_product" enctype="multipart/form-data">
-                            <input name="_method" type="hidden" value="PATCH">
-                            @csrf
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <label for="name" class="col-sm-2 control-label">{{trans('label.Name')}}: <?=$required_span; ?></label>
-                                        <div class="col-sm-9">
-                                            <input type="text" class="form-control {!! $errors->first('name', 'error') !!}" name="name" id="name" value="{{$row['name']}}">
-                                            {!! $errors->first('name', '<label class="error">:message</label>') !!}
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- col-lg-6 -->
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <label for="desc" class="col-sm-2 control-label">{{trans('label.Desc')}}: <?=$required_span; ?></label>
-                                        <div class="col-sm-10">
-                                        <textarea type="text" name="desc" id="desc" >{{$row['desc']}}</textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- col-lg-12 -->
-                            </div>
-                            
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <label for="image_name" class="col-sm-2 control-label">{{trans('label.Images')}}:</label>
-                                        <div class="col-sm-10">
-                                            <?php 
-                                                if($row['image_name'] != ""){ 
-                                                    echo '<image src="'.asset('page_images').'/'.$row['image_name'].'" class="img-responsive mb-4" height="600px" width="500px">';
-                                                }
-                                            ?>
-                                            <input type="file" class="form-control" name="image_name" id="image_name">
-                                            <input type="hidden" value="{{$row['image_name']}}" class="form-control" name="image_name_hidden" id="image_name_hidden">
-                                            
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- col-lg-12 -->
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <div class="col-sm-10 col-sm-offset-2">
-                                            <button type="submit" class="btn btn-primary btn-flat submit">{{trans('label.Update')}}</button>
-                                            <a href="{{url('/admin')}}" class="btn btn-flat btn-default">{{trans('label.Back')}}</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                <button type="submit"  class="btn btn-primary  submit">Save</button>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn" data-bs-dismiss="modal">Close</button>
         </div>
     </div>
 </div>
-@endsection
 
-@section('script')
 <script>
-CKEDITOR.replace( 'desc' );
-
-    
-$(".add_product").validate({
-    submitHandler(form){
+$(function(){
+    $(".editCategory").validate({
+        submitHandler(form){
         $(".submit").attr("disabled", true);
-        var form_cust = $('.add_product')[0]; 
-        let form1 = new FormData(form_cust);
-        var desc = CKEDITOR.instances.desc.getData();
-        form1.append('desc',desc);
+        var form_data = $('.editCategory')[0];
+        var form1 = new FormData(form_data);
         $.ajax({
             type: "POST",
-            url: '{{route('admin.pages.update',$row['id'])}}',
-            data: form1,//$('.add_product').serialize(),
+            url: '{{route('admin.categories.update',$row->id)}}',
+            data: form1,
             contentType: false,
             processData: false,
             success: function( response ) {
                 if(response.error == 0){
                     toastr.success(response.msg);
+
                     setTimeout(function(){
-                        // location.href = '{{url('admin/pages')}}';
-                        location.reload();
-                    },2000);
+                        location.href = '{{url('admin/categories')}}';
+                    },1900);
                 }else{
                     $(".submit").attr("disabled", false);
                     toastr.error(response.msg);
@@ -127,10 +56,13 @@ $(".add_product").validate({
                     $(ele).addClass('error');
                     $('<label class="error">'+ value +'</label>').insertAfter(ele);
                 });
-          }
+            }
         })
         return false;
     }
 });
+    })
+
 </script>
-@stop
+
+
